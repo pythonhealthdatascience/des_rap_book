1abfeaac67908419a7360e987fa4228c10e2d20e
amyheather <a.heather2@exeter.ac.uk>
2025-06-16 16:44:40 +0100

    feat(param_valid): rewrote intro, and wrote R section on param validation with class

---
+++ pages/inputs/parameters_validation_resources/ParamClass.R
@@ -0,0 +1,24 @@
+#' @title Returns transfer_prob for validation example.
+#'
+#' @field transfer_prob Numeric. Transfer probability (0-1).
+
+ParamClass <- R6Class(
+  public = list(
+    transfer_prob = NULL,
+
+    #' @description
+    #' Initialises the R6 object.
+
+    initialize = function(
+        transfer_prob = 0.3
+    ) {
+      self$transfer_prob <- transfer_prob
+    }
+  )
+)
+
+# Create instance of ParamClass
+params <- ParamClass$new()
+
+# Mistype transfer_prob
+try(params$transfer_probs <- 0.4)
