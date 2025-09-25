f2f7ad67afc4a5c71444bae57fc8a6d3a7a18f91
amyheather <a.heather2@exeter.ac.uk>
2025-09-25 15:56:42 +0100

    style(lint): modifying linter settings

--- lint.sh
+++ lint.sh
@@ -6,10 +6,9 @@ print_section() {
     echo "--------------------------------------------------------------------"
 }
 
-# Note: I have used ```{r} #| file: file.R``` instead of
-# ```{r}{{< include file.R >}}```, and likewise for python, as the latter
-# breaks lintr (false positive messages, and missing other messages) and breaks
-# pylint (returns an error Parsing failed: 'invalid syntax'). It doesn't break
+# Note: For R, I have used ```{r} #| file: file.R``` instead of
+# ```{r}{{< include file.R >}}```, as the latter breaks lintr (false positive
+# messages, and missing other messages) and breaks. It doesn't break
 # if used in non-active code chunks as linters ignore those.
 
 print_section "R" "index.qmd"
@@ -26,6 +25,9 @@ echo "--------------------------------------------------------------------"
 print_section "python" "index.qmd and pages/"
 lintquarto -l pylint flake8 -p index.qmd pages/
 
-print_section "python" "tests/"
-pylint pages tests --ignore=linting_resources
-flake8 pages tests --exclude linting_resources
+print_section "python" "pages/ and tests/"
+
+pylint pages tests --ignore=linting_resources,outputs_resources,replications_resources
+pylint pages/output_analysis/outputs_resources pages/output_analysis/replications_resources --disable=missing-module-docstring,undefined-variable
+
+flake8 pages tests --exclude linting_resources,replications_resources
